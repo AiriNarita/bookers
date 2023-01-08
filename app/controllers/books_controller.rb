36943798ad580b345c944ec341c20fdda49f0857
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-08 08:37:34 +0000

    [add]index and show/edit/destroy bottom

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
-      
+    book = Book.find(params[:id])
+    book.show
+    redirect_to '/books/show'
   end
 
   def edit
+    book = Book.find(params[:id])
+    book.edit
+    redirect_to '/books/edit'
   end
 
   def delete
-  end 
+  end
 
-  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
 
-end  
+
+end
