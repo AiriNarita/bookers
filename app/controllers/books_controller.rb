eb329e83bcda0aa1f8406f2bc6d87877380b92fc
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-09 03:24:12 +0000

    [add]creat action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,12 @@ class BooksController < ApplicationController
    @Book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
 
   def index
     @books = Book.all
@@ -30,5 +36,13 @@ class BooksController < ApplicationController
     redirect_to '/books'
   end
 
+    
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 
 end
+
