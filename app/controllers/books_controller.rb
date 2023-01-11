aab2121d83a7aa4e5b0c453e3f75a73327aa3010
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-11 08:07:07 +0000

    [add]edit validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,14 +27,19 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(book.id)
-    
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
   end
 
 
